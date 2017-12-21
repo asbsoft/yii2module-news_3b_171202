4d1b9438c3e6995a48aafba2bca625041ce4568a
asbsoft <ab2014box@gmail.com>
2017-12-21 13:38:42 +0200

    init - minimal inherited module-package

---
+++ Module.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace asb\yii2\modules\news_3b_171202;
+
+//use asb\yii2\modules\news_1b_160430\Module as ParentModule; // possible variant
+use asb\yii2\modules\news_2b_161124\Module as ParentModule;
+
+class Module extends ParentModule
+{
+}
---
+++ config/params.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'label'   => 'News manager 3-b',
+    'version' => '3b.171202',
+    'origin'  => 'news_3b_171202',
+];
