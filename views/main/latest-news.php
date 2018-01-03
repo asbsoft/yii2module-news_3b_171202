f43a65dfc98674a706339b4a2c36f3feb20dd966
asbsoft <ab2014box@gmail.com>
2018-01-04 00:11:04 +0200

    forgotten view file

---
+++ views/main/latest-news.php
@@ -0,0 +1,59 @@
+<?php
+    /* @var $this asb\yii2\common_2_170212\web\UniView */
+    /* @var $models array of asb\yii2\modules\news_3b_171202\models\News */
+
+    use yii\helpers\Html;
+
+
+    $thumbnailWidth = '50px';
+    
+    $assets = $this->context->module->registerAsset('FrontAsset', $this);
+
+    $tc = $this->context->tcModule;
+
+    $lh = $this->context->module->langHelper;
+    $langCode = $lh::normalizeLangCode(Yii::$app->language);
+
+?>
+<div class="latest-news">
+    <?php if ($models): ?>
+        <h3><?= Yii::t($tc, 'Latest news') ?></h3>
+        <table class="table-bordered">
+        <tr style="vertical-align: top">
+        <?php foreach ($models as $model): ?>
+            <td>
+                <?php
+                    if(!empty($model->image)) {
+                        $imgSrc = $this->context->uploadsNewsUrl . '/' . $model->image;
+                    } else {
+                        $imgSrc = $assets->baseUrl . '/img/no-picture.jpg';
+                      //$imgSrc = false;
+                    }
+                ?>
+                <?php if ($imgSrc) {
+                          echo Html::img($imgSrc, [
+                              'class' => 'thumbnail pull-left latest-news-img',
+                              'style' => "width: {$thumbnailWidth}",
+                          ]);
+                      } ?>
+
+                <span class="latest-news-link">
+                    <?= Html::encode($model->title) ?>
+                    <?php
+                        $slug = $model->getSlug($langCode);
+                        if (empty($slug)) {
+                            $route = ['view', 'id' => $model->id];
+                        } else {
+                            $route = ['view-by-slug', 'slug' => $slug];
+                        }
+                    ?>
+                    <?= Html::a(Html::encode(">>>"), $route, [
+                            'title' => Html::encode($model->title),
+                        ]) ?>
+                </span>
+            </td>
+        <?php endforeach; ?>
+        </tr>
+        </table>
+    <?php endif; ?>
+</div>
